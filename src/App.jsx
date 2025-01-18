5f831dcd229619ce175ed8e0f26384da5208aaca
satyam <satyamsingh2001>
2025-01-18 18:26:30 +0530

    Bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import UseEffectExample from "./components/useEffectExample";
+import UseEffectExample from "./components/UseEffectExample";
 import AddCaptionPage from "./components/AddCaptionPage";
 import FinalCardPage from "./components/FinalCardPage";
 
